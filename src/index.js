9c8d31178ca2d67f0368cedd65c9fae0681bf23b
Atharv1104 <atharvpore2004@gmail.com>
2025-11-06 16:49:42 +0530

    login fix

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(cors({
     console.log('INCOMING ORIGIN:', origin);
     // allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
+    if (allowedOrigins.indexOf(origin) === -1 || typeof origin === 'undefined') {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
