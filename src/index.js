a944fc9407643b58b6ab3fbaf4a7757c47af1a7b
Atharv1104 <atharvpore2004@gmail.com>
2025-11-06 15:38:47 +0530

    test for origin cors

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
 ];
 app.use(cors({
   origin: function (origin, callback) {
+    console.log('INCOMING ORIGIN:', origin);
     // allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
